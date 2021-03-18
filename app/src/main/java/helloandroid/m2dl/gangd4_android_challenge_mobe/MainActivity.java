29c050efd3ada2519b3e549228b1a3d8fd2b18bd
MrChyps <56667988+MrChyps@users.noreply.github.com>
2021-03-18 17:54:13 +0100

    Initial commit

---
+++ app/src/main/java/helloandroid/m2dl/gangd4_android_challenge_mobe/MainActivity.java
@@ -0,0 +1,14 @@
+package helloandroid.m2dl.gangd4_android_challenge_mobe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
