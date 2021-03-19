2105019572816e05bc3611c5ac16706c148bf8ee
mrc2421a <charles.mordin@master-developpement-logiciel.fr>
2021-03-19 11:37:03 +0100

    feat(scores): add scores view

--- app/src/main/java/helloandroid/m2dl/gangd4_android_challenge_mobe/activities/MainActivity.java
+++ app/src/main/java/helloandroid/m2dl/gangd4_android_challenge_mobe/activities/MainActivity.java
@@ -2,16 +2,24 @@ package helloandroid.m2dl.gangd4_android_challenge_mobe.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import helloandroid.m2dl.gangd4_android_challenge_mobe.R;
+import helloandroid.m2dl.gangd4_android_challenge_mobe.services.ScoreService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Coucou");
+        ScoreService.initDatabase(this);
+        this.context = this;
+
+        startActivity(new Intent(context, ScoreActivity.class));
     }
 }
--- app/src/main/java/helloandroid/m2dl/gangd4_android_challenge_mobe/activities/ScoreActivity.java
+++ app/src/main/java/helloandroid/m2dl/gangd4_android_challenge_mobe/activities/ScoreActivity.java
@@ -2,15 +2,52 @@ package helloandroid.m2dl.gangd4_android_challenge_mobe.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageButton;
 
 import helloandroid.m2dl.gangd4_android_challenge_mobe.R;
+import helloandroid.m2dl.gangd4_android_challenge_mobe.adapter.ScoreListAdapter;
+import helloandroid.m2dl.gangd4_android_challenge_mobe.services.ScoreService;
 
 public class ScoreActivity extends AppCompatActivity {
+
+    private ScoreService scoreService;
+    private GridView scoreGridView;
+    private ScoreListAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_MASK_ADJUST);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_score);
+        scoreService = ScoreService.getInstance(this);
+        scoreGridView = findViewById(R.id.scoreGridView);
+        refreshAdapterList();
+    }
+
+    private void refreshAdapterList() {
+        myAdapter = new ScoreListAdapter(this, scoreService.getScores());
+        scoreGridView.setAdapter(myAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refreshAdapterList();
+    }
+
+    public void backToMainActivity(View v){
+        onBackPressed();
     }
 }
