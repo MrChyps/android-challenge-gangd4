1bfe7b90d4e80b8fe4c6b120b4e42328f944e235
MrChyps <56667988+MrChyps@users.noreply.github.com>
2021-03-19 09:02:20 +0100

    Added basic activities & ActionType Enum

---
+++ app/src/main/java/helloandroid/m2dl/gangd4_android_challenge_mobe/activities/GameActivity.java
@@ -0,0 +1,16 @@
+package helloandroid.m2dl.gangd4_android_challenge_mobe.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import helloandroid.m2dl.gangd4_android_challenge_mobe.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
--- app/src/main/java/helloandroid/m2dl/gangd4_android_challenge_mobe/activities/MainActivity.java
+++ app/src/main/java/helloandroid/m2dl/gangd4_android_challenge_mobe/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package helloandroid.m2dl.gangd4_android_challenge_mobe;
+package helloandroid.m2dl.gangd4_android_challenge_mobe.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import helloandroid.m2dl.gangd4_android_challenge_mobe.R;
 
 public class MainActivity extends AppCompatActivity {
 
